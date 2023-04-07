830eb67c7901cb75f9d16bdd36b4f902a246e912
olmeiro <olme.orozco@gmail.com>
2023-04-07 15:11:43 -0500

    profiles properties resources

---
+++ src/main/java/com/tienda/deunomarket/DeUnoMarket.java
@@ -0,0 +1,14 @@
+package com.tienda.deunomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeUnoMarket {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DeUnoMarket.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/deunomarket/DeunoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.deunomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeunoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
