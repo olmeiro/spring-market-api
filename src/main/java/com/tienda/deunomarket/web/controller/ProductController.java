a1266350929c288fbb1c9d7c88330afb2e93676e
olmeiro <olme.orozco@gmail.com>
2023-04-08 17:08:19 -0500

    Exposicion de nuestra api
    anotaciones metodos y colección postman

--- src/main/java/com/tienda/deunomarket/web/controller/ProductController.java
+++ src/main/java/com/tienda/deunomarket/web/controller/ProductController.java
@@ -3,6 +3,12 @@ package com.tienda.deunomarket.web.controller;
 import com.tienda.deunomarket.domain.Product;
 import com.tienda.deunomarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,30 +18,36 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-
-    //inyectamos ProductService
-    @Autowired //lo podemos hacer ya que Product service usa una notación Spring
+    @Autowired
     private ProductService productService;
 
-    //métodos que tenemos dentro del servicio:
+    @GetMapping("/all") //obteniendo info
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-
-    public Product save(Product product) {
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(int productId) {
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("ida") int productId) {
         return productService.delete(productId);
+    }
+
+    @PutMapping("/update")
+    public Product update(@RequestBody Product product){
+        return productService.update(product);
     }
 }
