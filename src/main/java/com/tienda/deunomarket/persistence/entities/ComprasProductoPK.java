720ebb033fc0f536cc10dfa987229127d6ed0859
olmeiro <olme.orozco@gmail.com>
2023-04-07 20:55:51 -0500

    feat: creacion de entities del proyecto

---
+++ src/main/java/com/tienda/deunomarket/persistence/entities/ComprasProductoPK.java
@@ -0,0 +1,31 @@
+package com.tienda.deunomarket.persistence.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable // Se usa la notación embeddable porque después se embede en la otra clase
+public class ComprasProductoPK implements Serializable {
+
+    @Column (name = "id_compra")
+    private Integer idCompra;
+
+    @Column (name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
